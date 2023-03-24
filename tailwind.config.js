18e317abab2f782c3caa2ad9ea8484c999dd439c
leopico <linkoko.peceng@gmail.com>
2023-03-24 08:06:34 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Barlow Semi Condensed, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
